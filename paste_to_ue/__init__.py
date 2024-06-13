8920b5b950cd418e029ef13a209d6f50da999939
joric <joric@users.noreply.github.com>
2024-06-13 09:59:31 +0500

    updated files

--- paste_to_ue/__init__.py
+++ paste_to_ue/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "paste_to_ue",
     "author": "joric",
-    "version": (0, 2),
+    "version": (0, 3),
     "blender": (3, 6, 0),
     "category": "Object",
     "location": "View 3D > Object",
@@ -15,6 +15,7 @@ import bpy
 from math import *
 import mathutils
 from mathutils import *
+from collections import *
 
 class CustomTabPanel1(bpy.types.Panel):
     bl_label = "Match Templates"
@@ -25,10 +26,8 @@ class CustomTabPanel1(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-
         layout.prop(context.scene, "custom_delta")
         layout.prop(context.scene, "custom_radio_selection", expand=True)
-
         layout.operator("custom.button_operator1", text="Create Instances")
 
 class CustomTabPanel2(bpy.types.Panel):
@@ -40,12 +39,146 @@ class CustomTabPanel2(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-
         layout.prop(context.scene, "path_to_blueprint")
         layout.prop(context.scene, "use_scale")
         layout.prop(context.scene, "custom_scale")
-
         layout.operator("custom.button_operator2", text="Copy to Clipboard")
+
+class CustomTabPanel3(bpy.types.Panel):
+    bl_label = "Auto Mesh Separation"
+    bl_idname = "MY_PT_CustomTabPanel3"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Paste to UE'
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("custom.button_operator3", text="Auto Mesh Separation")
+
+def align(template, cloud, ofs, obj, cloud_obj, scale=True):
+    source_points = template[:3]
+    target_points = cloud[ofs:ofs+3]
+
+    a,b,c = [[source_points[i],target_points[i]] for i in range(3)]
+    p = obj.location.copy(), a[1]
+
+    def calc_matrix(i):
+        x = (b[i] - a[i]).normalized()
+        z = x.cross(c[i] - a[i]).normalized()
+        y = z.cross(x).normalized()
+        w = p[i].copy()
+        w.resize_4d()
+        m = Matrix()
+        m[0], m[1], m[2], m[3] = x.resized(4), y.resized(4), z.resized(4), w
+        m.transpose()
+        return m
+
+    dest, src = calc_matrix(0), calc_matrix(1)
+    r = src @ dest.inverted() @ obj.matrix_world.copy()
+
+    if scale:
+        scale = (b[1] - a[1]).length / (b[0] - a[0]).length
+        r.transpose()
+        for i in range(3):
+            r[i].xyz *= scale
+        r.transpose()
+
+    snap = obj.matrix_world.inverted() @ a[0]
+    offset = r.to_translation() - r @ snap
+    parent = cloud_obj.matrix_world
+
+    obj.matrix_world = parent @ Matrix().Translation(offset) @ r
+
+def get_points(o):
+    points = []
+    for face in o.data.polygons:
+        for idx in face.vertices:
+            points.append( o.data.vertices[idx].co )
+    return points
+
+# Auto Mesh Separation
+class CustomButtonOperator3(bpy.types.Operator):
+    bl_idname = "custom.button_operator3"
+    bl_label = "Custom Button"
+
+    def execute(self, context):
+        import sys,imp
+
+        # merge vertices with some tolerance value
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_all(action = 'SELECT')
+        bpy.ops.mesh.remove_doubles(threshold=0.01)
+        bpy.ops.mesh.separate(type='LOOSE')
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+        # iterate all meshes, collect point data
+        objects = []
+        meshes = []
+        collection = defaultdict(list)
+        for o in bpy.context.selected_objects:
+            if o.type != 'MESH':
+                continue
+            meshes.append(o)
+            points = get_points(o)
+
+            # classify objects
+            def calc_dist(points, ofs, n):
+                full_dist = (points[ofs]-points[ofs + n-1]).length
+                half_dist = (points[ofs]-points[ofs + n//2]).length
+                return half_dist / full_dist if full_dist else 0
+
+            dist = calc_dist(points, 0, len(points))
+            key = floor(dist*10)/10
+
+            # let's just classify by number of points for now
+            key = len(points)
+
+            collection[key].append(o)
+
+        # ok we have collection of objects, let's make templates from the first item in list
+
+        for key in collection.keys():
+            template_obj = collection[key][0]
+
+            # duplicate object with geometry
+            obj = bpy.data.objects.new(name=template_obj.name+'_template', object_data=template_obj.data.copy())
+            bpy.context.collection.objects.link(obj)
+
+            # TODO: we need to align object to axes and set origin to the bottom
+            bpy.ops.object.select_all(action = 'DESELECT')
+            obj.select_set(True)
+            obj.matrix_world.identity()
+            bpy.ops.object.origin_set()
+
+            # let's align using existing tools
+            cloud = [Vector((0,0,0)), Vector((0,1,0)), Vector((1,0,0))]
+            template = get_points(obj)
+            align(template, cloud, 0, obj, obj, False)
+
+            # move origin to center of x,y plane, leave z intact
+            obj.location = Vector((0,0, obj.location.z))
+
+            # apply all transformations
+            bpy.context.view_layer.objects.active = obj
+            bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
+
+            template_obj = obj
+            template = get_points(obj)
+
+            for cloud_obj in collection[key]:
+                obj = bpy.data.objects.new(name=template_obj.name+'_instance', object_data=template_obj.data)
+                bpy.context.collection.objects.link(obj)
+                cloud = get_points(cloud_obj)
+                align(template, cloud, 0, obj, cloud_obj)
+
+        # cleanup
+        bpy.ops.object.select_all(action = 'DESELECT')
+        for o in meshes:
+            o.select_set(True)
+        bpy.ops.object.delete()
+
+        self.report({'INFO'}, f'finished, found {len(collection)} unique meshes')
+        return {'FINISHED'}
 
 # Match Templates
 class CustomButtonOperator1(bpy.types.Operator):
@@ -87,39 +220,6 @@ class CustomButtonOperator1(bpy.types.Operator):
 
         templates, cloud = objects[:-1], objects[-1]
 
-        def align(template, cloud, ofs, obj, cloud_obj):
-            source_points = template[:3]
-            target_points = cloud[ofs:ofs+3]
-
-            a,b,c = [[source_points[i],target_points[i]] for i in range(3)]
-            p = obj.location.copy(), a[1]
-
-            def calc_matrix(i):
-                x = (b[i] - a[i]).normalized()
-                z = x.cross(c[i] - a[i]).normalized()
-                y = z.cross(x).normalized()
-                w = p[i].copy()
-                w.resize_4d()
-                m = Matrix()
-                m[0], m[1], m[2], m[3] = x.resized(4), y.resized(4), z.resized(4), w
-                m.transpose()
-                return m
-
-            dest, src = calc_matrix(0), calc_matrix(1)
-            r = src @ dest.inverted() @ obj.matrix_world.copy()
-
-            scale = (b[1] - a[1]).length / (b[0] - a[0]).length
-            r.transpose()
-            for i in range(3):
-                r[i].xyz *= scale
-            r.transpose()
-
-            snap = obj.matrix_world.inverted() @ a[0]
-            offset = r.to_translation() - r @ snap
-            parent = cloud_obj.matrix_world
-
-            obj.matrix_world = parent @ Matrix().Translation(offset) @ r
-
         # calculate distance from first to last point of the object
         # then calculate distance from first to the middle point
         # calculate weighted dist = half_dist / full_dist
@@ -158,8 +258,6 @@ class CustomButtonOperator1(bpy.types.Operator):
             ofs += n
 
         self.report({'INFO'}, f'Matching finished, created {count} instances.')
-
-
         return {'FINISHED'}
 
 # Paste to Clipboard
@@ -211,10 +309,12 @@ def register():
         kmi = km.keymap_items.new(CustomButtonOperator2.bl_idname, 'C', 'PRESS', ctrl=True, shift=True)
         addon_keymaps.append((km, kmi))
 
+    bpy.utils.register_class(CustomTabPanel3)
     bpy.utils.register_class(CustomTabPanel1)
     bpy.utils.register_class(CustomTabPanel2)
     bpy.utils.register_class(CustomButtonOperator1)
     bpy.utils.register_class(CustomButtonOperator2)
+    bpy.utils.register_class(CustomButtonOperator3)
 
     bpy.types.Scene.custom_radio_selection = bpy.props.EnumProperty(
         items=[('INSTANCES', 'Instance', 'Create instances'),
@@ -235,8 +335,10 @@ def unregister():
 
     bpy.utils.unregister_class(CustomTabPanel1)
     bpy.utils.unregister_class(CustomTabPanel2)
+    bpy.utils.unregister_class(CustomTabPanel3)
     bpy.utils.unregister_class(CustomButtonOperator1)
     bpy.utils.unregister_class(CustomButtonOperator2)
+    bpy.utils.unregister_class(CustomButtonOperator3)
     del bpy.types.Scene.custom_text_input
 
 if __name__ == "__main__":
